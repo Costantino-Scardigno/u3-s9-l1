6add7bde3ca72673fe1dcf0bc4f9c81e42bc96d7
Costantino-Scardigno <costantino98@icloud.com>
2024-11-25 17:02:21 +0100

    second

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Button from './components/ButtonComponent';
 import Header from './Header';
+import ImageComponent from './components/ImageComponent';
 
 function App() {
   return (
     <div className="App">
      <Header logo={logo}  />
      <Button buttonText="Scopri di più" />
+     <ImageComponent style ={{width:700}} src= "https://cdn.motor1.com/images/mgl/pOAWo/s1/2020-bmw-x6.jpg" />
     </div>
 
   );
--- src/components/ImageComponent.jsx
+++ src/components/ImageComponent.jsx
@@ -4,7 +4,7 @@ import React from "react";
 class ImageComponent extends React.Component {
     render() {
         return (
-            <div><img src="" alt="" /></div>
+            <div><img style={this.props.style} src={this.props.src} alt = {this.props.alt} /></div>
         )
     }
 
